286ebb0a45587ed0da7141fba754ff75d4ecf331
David Craig Cothren <dacracot@yahoo.com>
2026-03-11 15:37:43 -0700

    CLI mess

--- src/org/dacracot/Solitaire.java
+++ src/org/dacracot/Solitaire.java
@@ -1,21 +1,47 @@
 package org.dacracot;
 //---------------------------------------------------
+import java.util.Scanner;
+//---------------------------------------------------
 public class Solitaire {
 	//-----------------------------------------------
 	public Solitaire(){}
 	//-----------------------------------------------
 	public static void main(String[] args){
-		int cards = 0;
-		int tries = 0;
-		if (args.length == 2){
-			cards = Integer.parseInt(args[0]);  // how many cards to flip
-			tries = Integer.parseInt(args[1]); 	// number of games to play
+		String params = String.join(" ",args);
+		int cards = 3;
+		int tries = 10;
+		boolean debug = false;
+System.out.println("params> "+params);
+		if (!params.isEmpty()){
+System.out.println("params.indexOf(--one)> "+params.indexOf("--one"));
+			if (params.indexOf("--one") != -1){
+				cards = 1;
+				}
+System.out.println("params.indexOf(--three)> "+params.indexOf("--three"));
+			if (params.indexOf("--three") != -1){
+				cards = 3;
+				}
+System.out.println("params.indexOf(--attempts)> "+params.indexOf("--attempts"));
+			if (params.indexOf("--attempts") != -1){
+				Scanner cli = new Scanner(params);
+				if ("--attempts".equals(cli.findInLine("--attempts"))){
+					tries = cli.nextInt();
+					}
+				}
+			debug = (params.indexOf("--debug") != -1);
 			}
 		else{
-			System.out.println();
-			System.out.println("parameters: cards games");
-			System.out.println();
+			System.out.println("usage: [--one | --three] [--attempts #] [--debug] ");
+			System.out.println("    --one: Turn only one card each play.");
+			System.out.println("    --three: Turn three cards each play.");
+			System.out.println("    --attempts: Number of games to attempt.");
+			System.out.println("    --debug: Verbose output about each game.");
+			System.out.println("");
 			}
+		System.out.println("");
+		System.out.println("running: turn "+cards+" cards and "+tries+" attempts "+(debug?"with":"without")+" debug");
+		System.out.println("");
+		//-------------------------------------------
 		for(int i=0; i<tries; i++){
  			Player player = new Player(cards);
  			player.run();
