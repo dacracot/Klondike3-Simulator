ede4629b81fe3476b0cd93f905dd7fa4a6c45feb
David Craig Cothren <dacracot@yahoo.com>
2026-03-12 13:22:35 -0700

    movement of cards from one game element to another

--- src/org/dacracot/Klondike.java
+++ src/org/dacracot/Klondike.java
@@ -33,47 +33,64 @@ public class Klondike{
 			}
 		}
 	//-----------------------------------------------
+	@Deprecated(forRemoval = true)
 	public boolean play(){
+System.err.println("play begin");
+// System.out.println("xxx > "+xxx);
 		stack.flip();
 		showAll("begin");
 		int moves = 0;
 		int anyMoves = 0;
 		int noMoves = 0;
+System.err.println("play ~~1~~");
 		while((stack.sizeDown() + stack.sizeUp()) > 0){
+System.err.println("play ~~2~~");
 			if (goal.playCard(stack.getUpCard())){
+System.err.println("play ~~3~~");
 				stack.removeUpCard();
 				moves++;
 				showAll("move stack to goal");
+System.err.println("play ~~4~~");
 				}
 			else{
+System.err.println("play ~~5~~");
 			
 			// if (board.playCard(stack.getUpCard())){
 			//    stack.removeUpCard();
 			//    moves++;
 			//    }
+System.err.println("play ~~6~~");
 			
 				if(stack.flip()){ //flopped
+System.err.println("play ~~7~~");
 					if (anyMoves == moves) {
+System.err.println("play ~~8~~");
 						noMoves++;
 						}
 					else{
+System.err.println("play ~~9~~");
 						anyMoves = moves;
 						noMoves = 0;
+System.err.println("play ~~10~~");
 						}
 					}
+System.err.println("play ~~11~~");
 				showAll("flip");
 				}
+System.err.println("play ~~12~~");
 			if (goal.winner()){
+System.err.println("play ~~13~~");
 				showAll("WINNER");
 				return(true);
 				}
 			else if(noMoves > 3){
+System.err.println("play ~~14~~");
 				showAll("LOSER");
 				return(false);
 				}
-// try{System.in.read();}catch(Exception e){}
+System.err.println("play ~~15~~");
 			}
-		System.err.println("Huh?");
+System.err.println("play end");
 		return(false);
 		}
 	//-----------------------------------------------
