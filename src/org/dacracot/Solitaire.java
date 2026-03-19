d7142ac94bd0bfe92cf004ab1ba4f00db1ab369d
David Craig Cothren <dacracot@yahoo.com>
2026-03-19 08:23:54 -0700

    random must be initialized for no parameter instance

--- src/org/dacracot/Solitaire.java
+++ src/org/dacracot/Solitaire.java
@@ -49,6 +49,7 @@ public class Solitaire {
 			System.out.println("    --debug: Verbose output about each game.");
 			System.out.println("    --seed: Random seed for repeatable play.");
 			System.out.println("");
+			Global.random = new Random(); // truly Random for no parameters
 			}
 		System.out.println("");
 		System.out.println("running: turn "+Global.cards+" cards and "+Global.tries+" attempts "+(Global.debug?"with":"without")+" debug "+(seeded?("with "+seed+" seed"):"without a seed"));
