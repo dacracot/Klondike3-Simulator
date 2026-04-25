d9db7811f016a6aa9a799558a7bf79aa2eff4714
David Craig Cothren <dacracot@yahoo.com>
2026-04-25 15:49:50 -0700

    run until killed and report what happened

--- src/org/dacracot/Global.java
+++ src/org/dacracot/Global.java
@@ -1,14 +1,19 @@
 package org.dacracot;
 //---------------------------------------------------
 import java.util.Random;
+import java.time.Instant;
 //---------------------------------------------------
 public class Global {
 	//-----------------------------------------------
+	public static Instant start = Instant.now();
 	public static int cards = 3;
 	public static int tries = 10;
+	public static int tried = 0;
+	public static int winner = 0;
 	public static Random random = null;
 	public static boolean debug = false;
 	public static boolean quiet = false;
+	public static boolean killed = false;
 	public static StringBuffer activeGame = new StringBuffer();
 	//-----------------------------------------------
 	}
