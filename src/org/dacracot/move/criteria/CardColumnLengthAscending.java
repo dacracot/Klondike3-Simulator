c18f690379fce126dc5e5c4053d63e8c1c086a00
David Craig Cothren <dacracot@yahoo.com>
2026-05-20 11:05:16 -0700

    new strategy implements sorting the choice of two playable cards, choose to expose face down cards as quickly as possible for b2b

--- src/org/dacracot/move/criteria/CardColumnLengthAscending.java
+++ src/org/dacracot/move/criteria/CardColumnLengthAscending.java
@@ -3,11 +3,11 @@ package org.dacracot.move.criteria;
 import java.util.Comparator;
 import org.dacracot.card.Card;
 //---------------------------------------------------
-public class CardColumnLength implements Comparator<Card> {
+public class CardColumnLengthAscending implements Comparator<Card> {
 	//-----------------------------------------------
 	@Override
 	public int compare(Card left, Card right) {
-		return Integer.compare(left.getWeight(), right.getWeight());
+		return (Integer.compare(left.getWeight(), right.getWeight()));
 		}
 	//-----------------------------------------------
 }
--- src/org/dacracot/table/Board.java
+++ src/org/dacracot/table/Board.java
@@ -65,10 +65,8 @@ public class Board {
 		for(int i=0; i<SEVEN; i++) {
 			for(Card card : columns.get(i)) {
 				if (!card.isHidden()) {
-
-// set weight here
-// card.setWeight(columns.length());
-
+					// set weight
+					card.setWeight(columns.get(i).size());
 					up.add(card);
 					break;
 					}
